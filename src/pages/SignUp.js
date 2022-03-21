44fe576640b150ec949b5242c2b03cbcd2dc4ea1
tarwat-uddin <t.uddin9121@gmail.com>
2022-03-21 13:04:38 +0000

    Fix bug in SignIn.js and SignUp.js pages
    Minor bug: import path set wrong

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -8,7 +8,7 @@ import { doc, setDoc } from "firebase/firestore";
 
 import { createUserWithEmailAndPassword } from "firebase/auth";
 
-import { db, auth } from "../../firebase";
+import { db, auth } from "../firebase";
 
 import {
   Container,
