bb4f540df8d7c94b0a3b0f34c009a0b958990794
tarwat-uddin <t.uddin9121@gmail.com>
2022-03-23 18:47:52 +0000

    Add HomeLayout.js layout

---
+++ src/layouts/HomeLayout/pages/Profile.js
@@ -0,0 +1,36 @@
+import { Grid } from "@mui/material";
+
+import MainSidebar from "../../../components/MainSidebar/MainSidebar";
+import UserProfile from "../../../components/User/UserProfile";
+
+function Profile(props) {
+  return (
+    <Grid
+      sx={{
+        flexWrap: "nowrap",
+        width: "100%",
+        height: {
+          xs: "100vh",
+          sm: "initial",
+        },
+        overflow: "hidden",
+      }}
+      container
+      direction={{
+        xs: "column",
+        sm: "row",
+      }}
+      spacing={{ xs: 2, md: 3 }}
+    >
+      <Grid item xs="auto" sm={6} md={4}>
+        <MainSidebar user={props.user} />
+      </Grid>
+
+      <Grid item xs="auto" sm={6} md={8}>
+        <UserProfile user={props.user} />
+      </Grid>
+    </Grid>
+  );
+}
+
+export default Profile;
