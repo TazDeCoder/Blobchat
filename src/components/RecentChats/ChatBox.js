e8c3d4e13a39b1593db35e5a521c7b54140f2672
tarwat-uddin <t.uddin9121@gmail.com>
2022-03-30 19:52:25 +0100

    Update RecentChat.js component
    Major significant changes

--- src/components/RecentChats/ChatBox.js
+++ src/components/RecentChats/ChatBox.js
@@ -1,33 +1,68 @@
-import { Link as RouterLink } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
-import { Box, Avatar, Typography, Link } from "@mui/material";
+import { Box, Avatar, Typography } from "@mui/material";
 
-import { stringAvatar } from "../../helpers";
+import { stringAvatar, fetchUsersByGroup } from "../../utils";
+
+import { fetchGroupById } from "../../store/group/group-actions";
+
+import StyledBadge from "../UI/StyledBadge";
 
 function ChatBox(props) {
+  const navigate = useNavigate();
+
+  const dispatch = useDispatch();
+
+  const [recipient, setRecipient] = useState({});
+
+  const user = useSelector((state) => state.user.currentUser);
+
+  useEffect(async () => {
+    if (user) {
+      const fetchedRecipients = await fetchUsersByGroup(props.group);
+      const filteredRecipients = fetchedRecipients.filter(
+        (recipient) => recipient.uid !== user.uid
+      );
+      setRecipient(...filteredRecipients);
+    }
+  }, [props.group, user, fetchUsersByGroup]);
+
+  const chatClickedHandler = () => {
+    dispatch(fetchGroupById(props.group.id));
+
+    navigate(`/home/chats/${props.group.id}`);
+  };
+
   return (
-    <Link
-      component={RouterLink}
-      to={`/home/chats/users/${props.title}`}
-      color="inherit"
-      underline="none"
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        border: "1px solid",
+        borderColor: "primary.light",
+        borderRadius: "9px",
+        cursor: "pointer",
+      }}
+      p={1}
+      my={1}
+      onClick={chatClickedHandler}
     >
-      <Box
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          bgcolor: "#c3ede7",
-          borderRadius: "9px",
-        }}
-        p={1}
-        my={1}
-      >
-        <Avatar {...stringAvatar(props.title)} />
-        <Typography variant="body2" mx={1}>
-          {props.title}
-        </Typography>
-      </Box>
-    </Link>
+      {Object.keys(recipient).length > 0 && (
+        <StyledBadge
+          overlap="circular"
+          anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+          variant="dot"
+          isOnline={recipient.isOnline}
+        >
+          <Avatar {...stringAvatar(recipient.username)} />
+        </StyledBadge>
+      )}
+      <Typography variant="body1" mx={1} color={"text.secondary"}>
+        {props.group.recentMessage?.text ?? "No recent messages"}
+      </Typography>
+    </Box>
   );
 }
 
--- src/components/RecentChats/RecentChats.js
+++ src/components/RecentChats/RecentChats.js
@@ -1,17 +1,21 @@
-import { collection, query, where } from "firebase/firestore";
-
-import { useCollectionData } from "react-firebase-hooks/firestore";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 import { Box, Typography } from "@mui/material";
 
-import { db } from "../../firebase";
+import { fetchGroupByUserId } from "../../store/group/group-actions";
 
 import ChatBox from "./ChatBox";
 
-function RecentChats(props) {
-  const roomsRef = collection(db, `rooms/users/${props.user.username}`);
-  const roomsQuery = query(roomsRef, where("isActive", "==", true));
-  const [rooms] = useCollectionData(roomsQuery);
+function RecentChats() {
+  const dispatch = useDispatch();
+
+  const authId = useSelector((state) => state.auth.id);
+  const groups = useSelector((state) => state.group.groups);
+
+  useEffect(() => {
+    dispatch(fetchGroupByUserId(authId));
+  }, [dispatch]);
 
   return (
     <Box
@@ -24,12 +28,10 @@ function RecentChats(props) {
       p={1}
     >
       <Typography variant="h5">Chats</Typography>
-      {rooms ? (
-        rooms.map((room) => (
-          <ChatBox key={Math.random().toString()} title={room.name} />
-        ))
+      {groups.length > 0 ? (
+        groups.map((group) => <ChatBox key={group.id} group={group} />)
       ) : (
-        <p>No chats.</p>
+        <Typography variant="body1">No recent chats.</Typography>
       )}
     </Box>
   );
